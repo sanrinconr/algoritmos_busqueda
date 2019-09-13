f63f8d19d1d8c62fe174e045fbc140211ae5a303
s099 <sanrinconr@correo.udistrital.edu.co>
2019-09-12 20:24:01 -0500

    Se agrega el algoritmo radix
    Solo se copio y pego de https://www.geeksforgeeks.org/radix-sort/

--- Cronometros/windows.cpp
+++ Cronometros/windows.cpp
@@ -2,25 +2,25 @@
 #include <iostream>
 #include <windows.h>
 
+double performancecounter_diff(LARGE_INTEGER *a, LARGE_INTEGER *b);
+
+
+
 int main() {
 
+  LARGE_INTEGER t_ini, t_fin;
+  double secs;
+  QueryPerformanceCounter(&t_ini);
 
-    LARGE_INTEGER frequency;
-    LARGE_INTEGER start;
-    LARGE_INTEGER end;
-    double interval;
+  //EL CODIGO
+  QueryPerformanceCounter(&t_fin);
+  secs = performancecounter_diff(&t_fin, &t_ini);
+	printf("%.15g milliseconds\n", secs * 1000.0);
 
-    QueryPerformanceFrequency(&frequency);
-    QueryPerformanceCounter(&start);
+}
 
-    //EL CODIGO
-    QueryPerformanceCounter(&end);
-    interval = (double) (end.QuadPart - start.QuadPart) / frequency.QuadPart;
-
-    std::cout<<n<<",";
-    printf("%f\n", interval);
-
-  }
-
-
+double performancecounter_diff(LARGE_INTEGER *a, LARGE_INTEGER *b) {
+  LARGE_INTEGER freq;
+  QueryPerformanceFrequency(&freq);
+  return (double)(a->QuadPart - b->QuadPart) / (double)freq.QuadPart;
 }
